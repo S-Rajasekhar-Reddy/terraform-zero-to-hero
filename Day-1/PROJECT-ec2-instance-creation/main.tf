529c9fd92faf3a8336b74f944db6d922dd7a9ef1
Sai Rajasekhar Reddy Tammineni <rajasekharreddyt4@gmail.com>
2026-01-13 19:00:16 -0800

    working ec2 creation script, added respective git ignore file and also restrucured the day-1 content

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    ami           = "ami-0f5fcdfbd140e4ab7"
+    instance_type = "t3.micro"
 }
