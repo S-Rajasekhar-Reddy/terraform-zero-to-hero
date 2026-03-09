6962e6ecbe495b072605c4fa9f6fe90b63ffb8c1
Sai Rajasekhar Reddy Tammineni <rajasekharreddyt4@gmail.com>
2026-03-08 20:50:40 -0400

    Day-4 backend corrections, Day-5 hosting a web app with ec2 and public vpc with provisioners file and remote, Day-6 module implementation with workspaces

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -1,24 +1,21 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
-variable "ami" {
-  description = "value"
-}
+data "aws_region" "current" {}
 
 variable "instance_type" {
-  description = "value"
-  type = map(string)
-
+  description = "Map of instance types per workspace"
+  type        = map(string)
   default = {
-    "dev" = "t2.micro"
-    "stage" = "t2.medium"
-    "prod" = "t2.xlarge"
+    "dev"   = "t3.micro"
+    "stage" = "t2.micro"
+    "prod"  = "t3.micro"
   }
 }
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami = var.ami
+  ami = data.aws_region.current.id == "us-east-1" ? "ami-053b0d53c279acc90" : "ami-06e3c045d79fd65d9"
   instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
 }
